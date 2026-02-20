2006e8f5fcc3be96470f9faa412418e00823b245
YacobArima <yacobarima@gmail.com>
2026-02-20 14:49:47 +0700

    Initial commit: roomify project setup

---
+++ app/routes/home.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/home";
+import Navbar from "../../components/Navbar";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+      <div className="Home">
+        <Navbar/>
+        <h1 className="text-3xl font-extrabold text-indigo-700">Home</h1>
+      </div>
+  )
+}
